b92a438f1ebdd04b71cbd287e147fb8c76e22d9d
viral <viral@fosserp.com>
2024-06-13 04:53:44 +0000

    ch

---
+++ jkm/jkm/doc_events/lead.py
@@ -0,0 +1,17 @@
+import frappe
+from frappe.utils import get_link_to_form
+
+
+def validate(self, method):
+    data = frappe.db.sql(f"""
+            Select name
+            From `tabLead`
+            where status not in ('Opportunity', 'Quotation') and custom_item_group = '{self.custom_item_group}'
+            and (email_id = '{self.email_id}' or custom_mobile_number = '{self.custom_mobile_number}')
+    """, as_dict = 1)
+
+    if data:
+        message = "Inquiry is allready exist in the system for item group {0} please check the link mentioned below".format(self.item_group)
+        for row in data:
+            message += "<br>"
+            message += "<p>{0}</p>".format(get_link_to_form("Lead" , row.name))
